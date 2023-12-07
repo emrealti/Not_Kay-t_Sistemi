775b0508fa4ce6e1f436144c8fe7e270182a5e6f
emrealti <365912@ogr.ktu.edu.tr>
2023-12-07 23:27:08 +0300

    Add project files.

---
+++ FrmGiris.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Not_Kayıt_Sistemi
+{
+    public partial class FrmGiris : Form
+    {
+        public FrmGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmOgrenciDetay frm = new FrmOgrenciDetay();
+            frm.numara = maskedTextBox1.Text;
+            frm.Show();
+        }
+
+        //private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        //{
+        //    if(maskedTextBox1.Text=="1111")
+        //    {
+        //        FrmOgretmenDetay fr= new FrmOgretmenDetay();
+        //        fr.Show();
+        //    }
+        //}
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text == "1111")
+            {
+                FrmOgretmenDetay fr = new FrmOgretmenDetay();
+                fr.Show();
+            }
+        }
+
+}    
+}
---
+++ FrmOgretmenDetay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Not_Kayıt_Sistemi
+{
+    public partial class FrmOgretmenDetay : Form
+    {
+        public FrmOgretmenDetay()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmOgretmenDetay_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
